f0c1f593c3af9ccbfa173dc23508f16df49cc884
clarasenx <clarahelenasena@gmail.com>
2024-05-16 10:06:44 -0400

    mudanças no script e css

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const taskInput = document.getElementById("taskInput")
-const taskList
+const input = document.getElementById("taskInput");
+const ul = document.getElementById("taskList");
+
+function addTask() {
+  if (input.value !== "") {
+    const novaLi = document.createElement("li");
+
+    const removerBotao = document.createElement("button");
+    removerBotao.textContent = "Remover!";
+    removerBotao.onclick = function () {
+      ul.removeChild(novaLi);
+    };
+    const tarefa = document.createElement("p");
+    tarefa.className = "tarefa";
+    tarefa.textContent = input.value;
+    input.value = "";
+    novaLi.appendChild(tarefa);
+    novaLi.appendChild(removerBotao);
+    ul.appendChild(novaLi);
+  } else {
+    alert("Por favor, insira uma tarefa!");
+  }
+}
